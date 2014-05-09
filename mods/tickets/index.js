1ca5709ac0fd88daf21518a27021851d10d14d63
nitreo <sinitreo@gmail.com>
2014-05-09 18:03:23 +0200

    tickets progress

--- mods/tickets/index.js
+++ mods/tickets/index.js
@@ -1,8 +1,13 @@
 GameEvents.on("connect",function(){
   setInterval(function(){
-     RCON.send('getRules().SetGlobalMessage("FART FART FART");');
+     //RCON.send('getRules().SetGlobalMessage("FART FART FART");');
   },1000)
 });
 
 GameEvents.on("player_killed", function (victim, attacker) {
 });
+
+
+Parser.onRegex('^(LOADING|GENERATING)',function(){
+  console.log("RESTARTING RULES HERE");
+})
